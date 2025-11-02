187ecd49605d26849d789b7f36f65c82ffde9c66
Mellenker <melker.stafverfeldt@outlook.com>
2025-11-02 16:29:37 +0100

    Updated screenshot and adds 2 segments per apple

--- src/Game.cpp
+++ src/Game.cpp
@@ -81,6 +81,8 @@ bool Game::tryUpdateSnakeState() {
 	}
 	
 	if (detectAppleCollision(nextHeadPos)) {
+		// Add 2 segments and a point
+		m_snake.addSegment();
 		m_snake.addSegment();
 		m_scoreCounter.addPoint();
 
